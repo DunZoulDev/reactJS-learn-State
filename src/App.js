d9402b6066b47e799a8e2e9fc97068dfcef636dc
Dunlayawit Panklang <dunlayawit.dev@gmail.com>
2022-09-13 21:11:20 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    
+    
+    let count = 0
+    function plus(){
+      count++;
+    }
+
+    return (
+      <div>
+        {/* This is comment in JSX */}
+        <h1>Hello My first web page</h1>
+        <h1>Header {plus()}</h1>
+        
+        <span>{count}</span>
+    
+      </div>
+    )
+  }
 }
-
-export default App;
