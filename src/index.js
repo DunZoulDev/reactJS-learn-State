29eca7c68b09dbb618471cbcee84c231ef6b432c
Dunlayawit Panklang <dunlayawit.dev@gmail.com>
2022-09-22 17:21:45 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import App_Ex1 from './App_Ex1_Component_Stateless';
 import App_Ex2 from './App_Ex2_props';
+import App_Ex3 from './App_Ex3_Event_Props';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,7 +12,8 @@ root.render(
   <React.StrictMode>
     {/* <App/> */}
     {/* <App_Ex1/> */}
-    <App_Ex2/>
+    {/* <App_Ex2/> */}
+    <App_Ex3/>
   </React.StrictMode>
 );
 
